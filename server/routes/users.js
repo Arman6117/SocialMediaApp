531aecd090f5e74dab01ded5057f996796dbc0c5
Arman Patel <vanixgaming634@gmail.com>
2023-04-18 21:55:48 +0530

    Added logic of getting info friends and adding and removing them from the friend list

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,3 +5,15 @@ import {
     addRemoveFriend,
 } from "../Controllers/users.js";
 import {verifyToken} from "../middleware/auth.js"
+
+const router = express.Router();
+
+// READ
+router.get("/:id", verifyToken,getUser);  // Getting information of user
+
+router.get("/:id/:friendId", verifyToken,getUserFriends); // Getting information of user's friends
+
+// UPDATE
+router.patch("/:id/:friendId", addRemoveFriend);
+
+export default router;
